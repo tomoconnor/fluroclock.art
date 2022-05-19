3b60668b95f4682a0be1b2245c56e49b7b6007eb
Tom O'Connor <tom@twinhelix.org>
2022-05-19 23:46:47 +0100

    Updated Favicon

--- src/components/ClockModeToggle.js
+++ src/components/ClockModeToggle.js
@@ -32,24 +32,28 @@ const ClockModeToggle = () => {
 
         console.log(enabled);
     };
-    
-    React.useEffect(() => {
+
+      React.useEffect(() => {
         (async () => {
+          const interval = setInterval(async () => {
               try{
                   
-                  const response = await fetch ("http://192.168.186.12:9000/clock/isenabled");
-                  const responseJson = await response.json();
-                  setEnabled(responseJson.clock_mode)
+                const response = await fetch ("http://192.168.186.12:9000/clock/isenabled");
+                const responseJson = await response.json();
+                console.log("ClockModeToggle: " + responseJson.clock_mode);
+                setEnabled(responseJson.clock_mode)
               } catch (error) {
                   console.log(error);
               }
+          }, 1000);
+          return () => clearInterval(interval);
         })();
       }, []);
 
   return (
     <div className="flex justify-center px-4 py-4">
         <div className="form-check form-switch">
-            <input className="form-check-input appearance-none w-9 -ml-10 rounded-full float-left h-5 align-top bg-white bg-no-repeat bg-contain bg-gray-300 focus:outline-none cursor-pointer shadow-sm" type="checkbox" role="switch" id="flexSwitchCheckDefault" onChange={toggle} />
+            <input className="form-check-input appearance-none w-9 -ml-10 rounded-full float-left h-5 align-top bg-white bg-no-repeat bg-contain bg-gray-300 focus:outline-none cursor-pointer shadow-sm" type="checkbox" role="switch" id="flexSwitchCheckDefault" onChange={toggle} checked={enabled}/>
             <label className="form-check-label inline-block text-gray-800" htmlFor="flexSwitchCheckDefault">Clock Mode</label>
     </div>
 </div>
